0e7f497bfaa9d20253f114476240f20db070ebad
Antony Marcano <antony+github@antonymarcano.com>
2011-06-10 10:36:20 +0100

    Separating things out (in a way that works on 1.8.7 and 1.9.2)

---
+++ rake/version_auditor.rb
@@ -0,0 +1,23 @@
+def integer_from rational_version_number 
+  rational_version_number.delete(".").to_i
+end
+
+def current_ruby_version_is desired_ruby_version
+  your_version    = integer_from( RUBY_VERSION )
+  desired_version = integer_from desired_ruby_version
+
+  your_version >= desired_version
+end
+
+def check_ruby_version_is desired_ruby_version
+  unless current_ruby_version_is desired_ruby_version
+    $stderr.puts "Oops! You are using ruby #{RUBY_VERSION}. We really want you to use ruby #{desired_ruby_version} or higher.\n" + 
+      "Please upgrade to #{desired_ruby_version} or higher.\n" +
+      "Suggestion:\n" +
+      "  We like using rvm to manage our ruby versions.\n" +
+      "  That way, you can just say  `rvm use #{desired_ruby_version}`  and hey presto, you're on version #{desired_ruby_version}\n" +
+      "  rvm:  http://beginrescueend.com/\n\n" +
+      "Hey, it's up to you, but once you're on ruby #{desired_ruby_version} let us know by running `rake` again.\n\n"
+  end
+  puts "You're using ruby #{RUBY_VERSION}. We're happy about that.\n\n" if current_ruby_version_is desired_ruby_version
+end
