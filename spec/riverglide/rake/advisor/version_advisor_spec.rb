4eb5239ced05cb940d504bf8d4a284524849aa98
Antony Marcano <antony+github@antonymarcano.com>
2011-06-11 10:45:42 +0100

    RubyVersionExpert can tell you when the ruby version is same or higher - crudely

--- spec/riverglide/rake/advisor/version_advisor_spec.rb
+++ spec/riverglide/rake/advisor/version_advisor_spec.rb
@@ -2,8 +2,17 @@ module RiverGlide
   module Rake
     module Advisor
       class RubyVersionExpert
-        def self.are_we_using desired_ruby_version
-          RUBY_VERSION == desired_ruby_version
+        def self.are_we_using desired_ruby_version, compared=:exactly
+          return RUBY_VERSION == desired_ruby_version if compared == :exactly
+          desired_version = desired_ruby_version.split('.').collect { |n| n.to_i }
+          if compared == :or_higher
+            current_version = RUBY_VERSION.split('.').collect { |n| n.to_i }
+            same_or_higher = []
+            (0..2).each do |i|
+              same_or_higher[i] = current_version[i] >= current_version[i]
+            end
+            same_or_higher.all?
+          end
         end
       end
     end
@@ -28,13 +37,22 @@ module RiverGlide
           RubyVersionExpert.are_we_using( desired_ruby_version ).should be_false
         end
 
-        it "tells you when the ruby version is the same or higher", :wip => true do
-          # RubyVersionExpert.are_we_using( desired_ruby_version, :or_higher )
+        it "tells you when the ruby version is the same or higher" do
+          desired_ruby_version = lower_than RUBY_VERSION
+          
+          RubyVersionExpert.are_we_using( desired_ruby_version, :or_higher ).should be_true
         end
 
         def higher_than version
           version_numbers = version.split( '.' ).collect {|n| n.to_i}
           version_numbers[2] = version_numbers[2]+1
+
+          version_numbers.join '.'
+        end
+
+        def lower_than version
+          version_numbers = version.split( '.' ).collect {|n| n.to_i}
+          version_numbers[2] = version_numbers[2]-1
 
           version_numbers.join '.'
         end
