953912f14953b667a0df3a58848f70784a477349
Antony Marcano <antony+github@antonymarcano.com>
2011-06-13 06:33:05 +0100

    Removing duplication in RubyVersionExpert

--- lib/riverglide/rake/advisor/ruby_version_advisor.rb
+++ lib/riverglide/rake/advisor/ruby_version_advisor.rb
@@ -6,14 +6,19 @@ module RiverGlide
         def self.are_we_using? desired_ruby_version, compared=:exactly
           return RUBY_VERSION == desired_ruby_version if compared == :exactly
           if compared == :or_higher
-            desired_version = desired_ruby_version.split('.').collect { |n| n.to_i }
-            current_version = RUBY_VERSION.split('.').collect { |n| n.to_i }
+            desired_version = version_numbers_from desired_ruby_version
+            current_version = version_numbers_from RUBY_VERSION
             same_or_higher = []
             (0..2).each do |i|
               same_or_higher[i] = current_version[i] >= desired_version[i]
             end
             same_or_higher.all?
           end
+        end
+
+        private
+        def self.version_numbers_from version
+          version.split('.').collect { |n| n.to_i }
         end
       end
     end
