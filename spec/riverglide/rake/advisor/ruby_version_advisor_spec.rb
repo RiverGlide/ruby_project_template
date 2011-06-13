00be03a32ed6c94842239f9d485e9e989b4a2d44
Antony Marcano <antony+github@antonymarcano.com>
2011-06-13 12:45:56 +0100

    Making sure this all works for major, minor and build numbers (needs refactoring)

--- spec/riverglide/rake/advisor/ruby_version_advisor_spec.rb
+++ spec/riverglide/rake/advisor/ruby_version_advisor_spec.rb
@@ -4,7 +4,8 @@ module RiverGlide
     module Advisor
       describe RubyVersionExpert do
         include RiverGlide::Rake::Advisor
-        
+        let( :positions ) {{:major => 0, :minor => 1, :build => 2 }}
+
         it "tells you when the ruby version is exactly right" do
           desired_ruby_version = RUBY_VERSION
 
@@ -13,29 +14,25 @@ module RiverGlide
         end
 
         it "tells you when the ruby version is not exactly right (too high)" do
-          desired_ruby_version = version :higher_than, RUBY_VERSION
+          positions.keys.each do |position|
+            desired_ruby_version = a_version :higher_than, RUBY_VERSION, position
 
-          RubyVersionExpert.are_we_using?( desired_ruby_version ).should be_false
-          RubyVersionExpert.are_we_using?( desired_ruby_version, :exactly ).should be_false
+            RubyVersionExpert.are_we_using?( desired_ruby_version ).should be_false
+            RubyVersionExpert.are_we_using?( desired_ruby_version, :exactly ).should be_false
+          end
         end
 
         it "tells you when the ruby version is not exactly right (too low)" do
-          desired_ruby_version = version :lower_than, RUBY_VERSION
+          desired_ruby_version = a_version :lower_than, RUBY_VERSION, :build
 
           RubyVersionExpert.are_we_using?( desired_ruby_version ).should be_false
           RubyVersionExpert.are_we_using?( desired_ruby_version, :exactly ).should be_false
         end
 
         it "tells you when we are using a ruby version that is (the same) or higher" do
-          desired_ruby_version = version :lower_than, RUBY_VERSION
+          desired_ruby_version = a_version :lower_than, RUBY_VERSION, :build
           
           RubyVersionExpert.are_we_using?( desired_ruby_version, :or_higher ).should be_true
-        end
-
-        it "tells you when we are not using a ruby version that is (the same) or higher" do
-          desired_ruby_version = version :higher_than, RUBY_VERSION
-
-          RubyVersionExpert.are_we_using?( desired_ruby_version, :or_higher ).should be_false
         end
 
         it "tells you when we are using a ruby version that is the same (or higher)" do
@@ -44,18 +41,26 @@ module RiverGlide
           RubyVersionExpert.are_we_using?( desired_ruby_version, :or_higher ).should be_true
         end
 
-        it "complains when when it doesn't recognise the version number" do
+        it "tells you when we are not using a ruby version that is (the same) or higher" do
+          desired_ruby_version = a_version :higher_than, RUBY_VERSION, :build
+
+          RubyVersionExpert.are_we_using?( desired_ruby_version, :or_higher ).should be_false
+        end
+
+        it "complains when when it doesn't understand the version number" do
           unrecognised_versions = ["1.9.a", "1.9.2a", "not a version", ".1.9.2", "1.9.2.1"]
 
-          unrecognised_versions.each do | text |
-            lambda {RubyVersionExpert.are_we_using?( text )}.should raise_error
+          unrecognised_versions.each do | unrecognised_version |
+            lambda {RubyVersionExpert.are_we_using?( unrecognised_version )}.should raise_error
           end
         end
 
-        def version higher_or_lower_than, version_number
-          relation = {:higher_than => :+, :lower_than => :-}
+        def a_version higher_or_lower_than, version_number, major_minor_or_build
+          alterations = {:higher_than => :+, :lower_than => :-}
+          altered_by = alterations[higher_or_lower_than]
+          position = positions[major_minor_or_build]
           version_numbers = version_number.split( '.' ).collect {|n| n.to_i}
-          version_numbers[2] = version_numbers[2].send relation[higher_or_lower_than], 1
+          version_numbers[position] = version_numbers[position].send altered_by, 1
           version_numbers.join '.'
         end
       end
