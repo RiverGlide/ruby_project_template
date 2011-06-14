a843b7ab81e692d0573bd3257792fe7a15032515
Antony Marcano <antony+github@antonymarcano.com>
2011-06-14 23:24:25 +0100

    Putting things in their place

--- rake/required_auditors.rb
+++ rake/required_auditors.rb
@@ -1,3 +1,3 @@
 require './rake/audit_tasks/you_need_the_right_ruby_version'
-require './rake/auditors/dependency_manager_auditor'
-require './rake/auditors/dependencies_auditor'
+require './rake/audit_tasks/you_need_our_dependency_manager'
+require './rake/audit_tasks/you_need_to_install_any_dependencies'
