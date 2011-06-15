66aca03ea39c6d53e5681d7f27f15cbe5fc826f4
Antony Marcano <antony+github@antonymarcano.com>
2011-06-15 11:00:47 +0100

    Version number implementation is easier to understand

--- lib/versionary/version_number.rb
+++ lib/versionary/version_number.rb
@@ -5,28 +5,25 @@ module Versionary
 
     attr_reader :major, :minor, :build
 
+    VERSION_PATTERN = /^\d+\.\d+\.\d+$/
+
     def self.of version
       self.new version
     end
 
     def initialize version
       raise NotAVersionComplaint.about version if not a_recognised? version
-      numbers = version.split( '.' ).collect { |n| n.to_i }
-      @major = numbers[0]
-      @minor = numbers[1]
-      @build = numbers[2]
+      number = infer_from version
+      @major = value_of :major, number
+      @minor = value_of :minor, number
+      @build = value_of :build, number
     end
 
     def <=> other_version
-      other_version = VersionNumber.of(other_version) if other_version.class == String
-      diff = []
-      diff[0] = self.major <=> other_version.major
-      diff[1] = self.minor <=> other_version.minor
-      diff[2] = self.build <=> other_version.build
-      diff.each do |v|
-        return v if v != 0
-      end
-      0
+      with_other_version = VersionNumber.of(other_version) if other_version.class == String
+      with_other_version = other_version if other_version.class == self.class
+      
+      compare(self, with_other_version)
     end
 
     def to_s
@@ -35,7 +32,31 @@ module Versionary
 
     private
     def a_recognised? version
-      version =~ /^\d+\.\d+\.\d+$/
+      version =~ VERSION_PATTERN
+    end
+
+    def infer_from version
+      version.split( '.' ).collect { |n| n.to_i }
+    end
+
+    def value_of position, from_numbers
+      positions = {:major => 0, :minor => 1, :build => 2}
+      from_numbers[positions[position]]
+    end
+
+    def compare first_version, second_version
+      differences = []
+      differences[0] = first_version.major <=> second_version.major
+      differences[1] = first_version.minor <=> second_version.minor
+      differences[2] = first_version.build <=> second_version.build
+      differences.each do |indication|
+        return indication if it_is_a_greater_than_or_less_than? indication
+      end
+      0
+    end
+
+    def it_is_a_greater_than_or_less_than? indication
+      indication != 0
     end
   end
 
